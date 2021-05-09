61cd15d2e1913c29e2525d99bfb4a884b541783f
Sarojkatwal <katwalsaroj11@gmail.com>
2021-05-09 23:28:14 +0545

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Main from "./Root/main";
+import { HashRouter } from "react-router-dom";
 class App extends Component {
   render() {
-    return <Main />;
+    return (
+      <HashRouter>
+        <Main />
+      </HashRouter>
+    );
   }
 }
 
